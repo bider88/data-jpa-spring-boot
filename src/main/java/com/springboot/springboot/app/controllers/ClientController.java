c10cf612576972ea7ff85a1c1a763f9d57f32440
Sarmiento <Didier.Sarmiento.consultant@nielsen.com>
2018-11-13 23:41:54 -0600

    Creando el controlador y el método que hará el acceso GET

---
+++ src/main/java/com/springboot/springboot/app/controllers/ClientController.java
@@ -0,0 +1,23 @@
+package com.springboot.springboot.app.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.springboot.springboot.app.models.dao.IClientDAO;
+
+@Controller
+public class ClientController {
+	
+	@Autowired
+	private IClientDAO clientDAO;
+	
+	@RequestMapping(value="/list", method=RequestMethod.GET)
+	public String list(Model model) {
+		model.addAttribute("title", "List of clients");
+		model.addAttribute("clients", clientDAO.findAll());
+		return "Listar";
+	}
+}
