47fa27d109255005410344f3c4630267460a1598
Irving Didier <bider88@gmail.com>
2019-05-15 22:53:38 -0500

    Agregando paginador desde cero

--- src/main/java/com/springboot/springboot/app/models/service/ClientService.java
+++ src/main/java/com/springboot/springboot/app/models/service/ClientService.java
@@ -2,10 +2,15 @@ package com.springboot.springboot.app.models.service;
 
 import java.util.List;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
 import com.springboot.springboot.app.models.entity.Client;
 
 public interface ClientService {
 	public List<Client> findAll();
+	
+	public Page<Client> findAll(Pageable pageable);
 	
 	public void save(Client client);
 	
