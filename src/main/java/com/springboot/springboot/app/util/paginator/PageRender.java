9686827d03dc128a4219e1aba4c214904b7684b6
Irving Didier <bider88@gmail.com>
2019-06-01 13:56:50 -0500

    Fixing paginator

--- src/main/java/com/springboot/springboot/app/util/paginator/PageRender.java
+++ src/main/java/com/springboot/springboot/app/util/paginator/PageRender.java
@@ -48,7 +48,7 @@ public class PageRender<T> {
 		}
 		
 		for (int i = 0; i < until; i++) {
-			pages.add(new PageItem(from, currentPage == from + 1));
+			pages.add(new PageItem(from + i, currentPage == from + i));
 		}
 	}
 
